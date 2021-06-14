1d10fa08d06d448a86fd150b39347ab8e1129567
bhushan pawar <pawarbhushan2793@gmail.com>
2021-06-14 07:30:39 +0530

    checkpoint 4 for the Movie Filter Card

---
+++ src/common/Gridlistbanner.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+
+import moviesData from '../../common/moviesData';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    transform: 'translateZ(0)',
+  },
+  title: {
+    color: 'white',
+  },
+  titleBar: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+
+}));
+
+function SingleLineGridList() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+    <GridList cellHeight={250} className={classes.gridList} cols={6}>
+        {moviesData.map((tile) => (
+          <GridListTile key={tile.id}>
+            <img src={tile.poster_url} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+             />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+}
+
+
+
+const url='';
+function TitlebarGridList() {
+  return (
+    <div className='root'>
+      <GridList cellHeight={350} className='gridList2' cols={4}>
+        {moviesData.map((tile) => (
+          <GridListTile key={tile.id}>
+            <a href={url}>
+              <img src={tile.poster_url} alt={tile.title} />
+            </a>
+            <GridListTileBar
+              title={tile.title}
+              subtitle={<span>Release Date: {tile.release_date.substring(0,10)}</span>}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+}
+
